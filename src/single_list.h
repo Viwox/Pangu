50e3c4aba98ea82599151f9ba009a3b13f0060f9
leonhong <codeblocker@gmail.com>
2010-04-02 20:43:41 +0800

    modify return value

--- src/single_list.h
+++ src/single_list.h
@@ -16,35 +16,16 @@ typedef struct single_list_t {
 	int data;
 } single_list_t;
 
-<<<<<<< HEAD
 typedef struct single_list_s {
 	size_t size;
 	single_list_t *header;
 } single_list_s;
 
 ssize_t single_list_create(single_list_s**);
-=======
-/*
- * return:
- *	0	-	success
- *	-1	-	failure
- */
-
-int single_list_create(single_list_t**);
->>>>>>> c8cc20e9aa511313d119137edf2dd339b7f248b6
 
 size_t single_list_size(single_list_s*);
 
-<<<<<<< HEAD
 ssize_t single_list_push(single_list_s*, void*);
-=======
-/*
- * return:
- *	0	-	success
- *	-1	-	failure
- */
-int single_list_push(single_list_t*, void*);
->>>>>>> c8cc20e9aa511313d119137edf2dd339b7f248b6
 
 ssize_t single_list_pop(single_list_s*);
 
