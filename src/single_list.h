cf929022147a00756a8e862295be60f301fe7c1b
Viwox <viwox@foxmail.com>
2010-04-03 10:03:55 +0800

    push_back pop_front front

--- src/single_list.h
+++ src/single_list.h
@@ -25,11 +25,11 @@ int single_list_create(single_list_t**);
 
 size_t single_list_size(single_list_t*);
 
-int single_list_push(single_list_t*, void*);
+int single_list_push_back(single_list_t*, void*);
 
-int single_list_pop(single_list_t*);
+int single_list_pop_front(single_list_t*);
 
-void* single_list_top(single_list_t*);
+void* single_list_front(single_list_t*);
 
 int single_list_destroy(single_list_t**);
 
