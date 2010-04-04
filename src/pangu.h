dded5db0e8c027e8637d706e1132760c7d62ae32
leonhong <codeblocker@gmail.com>
2010-04-04 19:36:54 +0800

    add error number

--- src/pangu.h
+++ src/pangu.h
@@ -1,9 +1,13 @@
 #ifndef _PANGU_H_
 #define _PANGU_H_
 
+#define PANGU_VERSION "Pangu version 1.0"
 #define PANGU_OK 0
 #define PANGU_ERROR -1
 #define PANGU_MEMORY_NOT_ENOUGH -2
+#define PANGU_OPEN_FILE_FAIL -3
+#define PANGU_WRITE_FILE_FAIL -4
+#define PANGU_STAT_FILE_FAIL -5
 #define pangu_malloc malloc
 #define pangu_free free
 
