5c26aa3ba4ffd5d9d8a6a8c4bfaacd35e387b59e
leonhong <codeblocker@gmail.com>
2010-04-05 12:13:05 +0800

    add set

--- src/pangu.h
+++ src/pangu.h
@@ -7,7 +7,9 @@
 #define PANGU_MEMORY_NOT_ENOUGH -2
 #define PANGU_OPEN_FILE_FAIL -3
 #define PANGU_WRITE_FILE_FAIL -4
-#define PANGU_STAT_FILE_FAIL -5
+#define PANGU_READ_FILE_FAIL -5
+#define PANGU_STAT_FILE_FAIL -6
+#define PANGU_SEEK_FILE_FAIL -7
 #define pangu_malloc malloc
 #define pangu_free free
 #define HDBIOBUF 1024
